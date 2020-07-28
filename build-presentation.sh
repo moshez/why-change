be3556b3d0c678617b080d53a6123d16ba6f4f73
Moshe Zadka <zadka.moshe@gmail.com>
2020-07-27 21:11:06 -0700

    +stuff

--- build-presentation.sh
+++ build-presentation.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+set -e
 jupyter nbconvert why-change.ipynb --to markdown --TagRemovePreprocessor.remove_cell_tags='{"no_markdown"}'  --output build/why-change.md
-pandoc -o build/why-change.tex build/why-change.md 
-pip install git+https://github.com/moshez/moshez_middlefield_beamer.git
+pandoc --listings -o build/why-change.tex build/why-change.md 
 mf beamer --input build/why-change.tex --outdir build
+(cd build && pdflatex talk.tex)
+(cd build && pdflatex handout.tex)
 
